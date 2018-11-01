948f2e7f32fe548aab32e4e93fb7f4b9985eb107
muhammaderlangga51 <muhammad.erlangga51@ui.ac.id>
2018-11-01 19:24:27 +0700

    init

---
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -0,0 +1,5 @@
+package com.apap.tutorial7.rest;
+
+public class Setting {
+	final public static String pilotUrl = "https://e40910f2-6a50-4bed-957a-def6158c26a3.mock.pstmn.io";
+}
---
+++ src/main/java/com/apap/tutorial7/service/FlightService.java
@@ -0,0 +1,19 @@
+package com.apap.tutorial7.service;
+
+import java.util.Optional;
+
+import com.apap.tutorial7.model.FlightModel;
+import com.apap.tutorial7.model.PilotModel;
+
+/**
+ * FlightService
+ */
+public interface FlightService {
+    FlightModel addFlight(FlightModel flight);
+    
+    void deleteByFlightNumber(String flightNumber);
+
+    Optional<FlightModel> getFlightDetailByFlightNumber(String flightNumber);
+    
+    Optional<FlightModel> getFlightDetailById(long id);
+}
